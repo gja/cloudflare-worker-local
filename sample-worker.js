80fb61e2cbdd778d7f4071afe72a1138b388bebb
Tejas Dinkar <tejas@gja.in>
2018-11-14 14:06:25 +0530

    Adding a sample worker

---
+++ sample-worker.js
@@ -0,0 +1,12 @@
+addEventListener('fetch', event => {
+  event.respondWith(fetchAndApply(event.request))
+})
+
+async function fetchAndApply(request) {
+  if (request.headers.get('user-agent').includes('annoying_robot')) {
+    return new Response('Sorry, this page is not available.',
+        { status: 403, statusText: 'Forbidden' })
+  }
+
+  return fetch(request)
+}
