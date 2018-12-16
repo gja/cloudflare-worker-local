b9b446a73f0a91de6f1f606d4025ee6374ce6311
Jeremy Danyow <jdanyow@gmail.com>
2018-12-15 19:24:22 -0800

    add delete method to kv store (#20)

--- app/in-memory-kv-store.js
+++ app/in-memory-kv-store.js
@@ -7,7 +7,8 @@ class InMemoryKVStore {
     this.values[namespace] = this.values[namespace] || {};
     return {
       get: async key => this.values[namespace][key],
-      put: async (key, value) => (this.values[namespace][key] = value)
+      put: async (key, value) => (this.values[namespace][key] = value),
+      delete: async key => delete this.values[namespace][key]
     };
   }
 }
